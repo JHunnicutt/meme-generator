6b6dc2c58df4b9bb4bfb77a5dd44037781c5c032
Jordan Hunnicutt <jordanneilhunnicutt@gmail.com>
2022-07-26 06:53:14 -0400

    add header

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header className='header'>
+            <img src='./Troll Face.png' className='header__logo' />
+            <h2>Meme Generator</h2>
+            <h4>React Course - Project 3</h4>
+        </header>
+    )
+}
