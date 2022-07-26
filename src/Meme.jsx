78ed77bb04629df20431f1ac01069664eb7452bf
Jordan Hunnicutt <jordanneilhunnicutt@gmail.com>
2022-07-26 17:57:15 -0400

    enable meme form functionality

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
+import memesData from './memesData'
 
 export default function Meme() {
+
+    const [memeImage, setMemeImage] = React.useState('')
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNum = Math.floor(Math.random() * (memesArray.length + 1))
+
+        setMemeImage(memesArray[randomNum].url)
+
+        
+    }
     return (
         <main>
-            <form className='meme__form'>
+            <div className='meme__form'>
                 <input type='text' className='meme__form-input' placeholder='Top text' />
                 <input type='text' className='meme__form-input' placeholder='Bottom text' />
-                <button className='meme__form-btn'>Get a new meme image 🖼️</button>
-            </form>
+                <button 
+                    className='meme__form-btn'
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼️
+                </button>
+            </div>
+            <img src={memeImage} className='meme__img' />
         </main>
     )
 }
